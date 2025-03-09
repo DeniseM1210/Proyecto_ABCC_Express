c2739444adc28d272cb66749c4dc5805f3d378d5
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2025-03-08 22:04:20 -0600

    Se comento la conexión a la base de datos local

--- config/database.js
+++ config/database.js
@@ -4,18 +4,18 @@ const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
     // --- local ---
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'BD_Express_2025'
+    //host: 'localhost',
+    //user: 'root',
+    //password: '',
+    //database: 'BD_Express_2025'
 
     // --- en linea ---
-    /*
+    
     host: 'sql10.freesqldatabase.com',
     user: 'sql10766658',
     password: '8FmwvyhmUM',
     database: 'sql10766658'
-    */
+    
 });
 
 conexion.connect(function(err){
